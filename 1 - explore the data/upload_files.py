4c2e68a8ba2eea0c099427939578b2aec4b1a512
a-fowler248 <amanda.fowler248@gmail.com>
2025-03-26 16:36:39 -0400

    clean the data in the python script based on the sql findings

--- 1 - explore the data/upload_files.py
+++ 1 - explore the data/upload_files.py
@@ -6,8 +6,13 @@ import pandas as pd
 
 # Load data files
 df_users = pd.read_csv('/Users/amandafowler/Documents/VSCode/Takehome_CSV_files/USER_TAKEHOME.csv')
-df_transactions = pd.read_csv('/Users/amandafowler/Documents/VSCode/Takehome_CSV_files/TRANSACTION_TAKEHOME.csv')
-df_products = pd.read_csv('/Users/amandafowler/Documents/VSCode/Takehome_CSV_files/PRODUCTS_TAKEHOME.csv')
+
+df_transactions = pd.read_csv('/Users/amandafowler/Documents/VSCode/Takehome_CSV_files/TRANSACTION_TAKEHOME.csv', dtype={'BARCODE': str}) # maintain barcode as string so we don't lose the leading zeros
+# additional cleaning added for barcode and:
+df_transactions['FINAL_QUANTITY'] = df_transactions['FINAL_QUANTITY'].replace('zero',0) # replace zero with 0
+df_transactions['FINAL_SALE'] = df_transactions['FINAL_SALE'].replace(' ',0) # replace blank cells with 0
+
+df_products = pd.read_csv('/Users/amandafowler/Documents/VSCode/Takehome_CSV_files/PRODUCTS_TAKEHOME.csv', dtype={'BARCODE': str}) # maintain barcode as string so we don't lose the leading zeros
 
 # Initial data clean up, take away any leading spaces
 df_users.columns = df_users.columns.str.strip()
@@ -19,9 +24,9 @@ connection = sqlite3.connect('takehome.db')
 
 # Load data files to SQLite
 ## set up to replace the table if it already exists - this will reflect any additional cleaning in the updated table
-df_users.to_sql('users', connection, if_exists='replace')
-df_transactions.to_sql('transactions', connection, if_exists='replace')
-df_products.to_sql('products', connection, if_exists='replace')
+df_users.to_sql('users', connection, if_exists='replace', index=False)
+df_transactions.to_sql('transactions', connection, if_exists='replace', index=False, dtype={'FINAL_QUANTITY': 'DECIMAL(10, 4)', 'FINAL_SALE': 'DECIMAL(10, 4)'}) # specifying to convert the float fields to decimals
+df_products.to_sql('products', connection, if_exists='replace', index=False)
 
 # Close the connection
 connection.close()
